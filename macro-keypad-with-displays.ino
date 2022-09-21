a87f8018bf41055466d692a9380471291d6e2492
Ben Harrison <benharrison52@outlook.com>
2022-09-20 21:03:27 -0400

    added delay

--- macro-keypad-with-displays.ino
+++ macro-keypad-with-displays.ino
@@ -415,6 +415,7 @@ void TCA9548A(uint8_t bus){
   Wire.beginTransmission(0x70);  // TCA9548A address is 0x70
   Wire.write(1 << bus);          // send byte to select bus
   Wire.endTransmission();
+  delay(100);
 }
 
 void configPositionManager(int pos) {
